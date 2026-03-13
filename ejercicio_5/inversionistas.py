0f3ea3be83f2d1d7d2f0bde28514fd6a0ba3dec7
dani72405bue <danielbueno@colegioguanenta.edu.co>
2026-03-13 07:15:01 -0500

    se actualizo el readme con analisis y diseño

--- ejercicio_5/inversionistas.py
+++ ejercicio_5/inversionistas.py
@@ -2,6 +2,15 @@
 import math 
 # input 
 
-C1 = int(input("introduzca el valor de capital de Juan"))
-C2 = int(input("ahora introduce el capital de Pedro"))
-C3 = 1000
+C1 = int(input("introduzca el valor de capital de Juan:" ))
+C2 = int(input("ahora introduce el capital de Pedro:" ))
+C3 = int(input("capital necesario para hacer el negocio:" ))
+
+meses = 0
+# proceso
+while C1 + C2 < C3:
+    C1 = C1 * 1.03
+    C2 = C2 * 1.04
+    meses = meses + 1 
+
+print("el negocio se hará en:", meses, "meses")
